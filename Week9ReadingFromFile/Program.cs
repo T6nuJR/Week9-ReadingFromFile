c0fdf754952aed36b5f6a8b09461906137f903df
Tonu Riisimae <example@mail.com>
2024-12-18 11:11:52 +0200

    Week9

--- Week9ReadingFromFile/Program.cs
+++ Week9ReadingFromFile/Program.cs
@@ -6,8 +6,7 @@ string fullFilePath = Path.Combine(fileDirectoryPath, fileName);
 
 ReadFromFile(fullFilePath);
 
-
-static void DisplayDataFromArray(string[] someArray)
+static void DisplayDataFromArry(string[] someArray)
 {
     foreach (string line in someArray)
     {
@@ -15,28 +14,20 @@ static void DisplayDataFromArray(string[] someArray)
     }
 }
 
-static void DisplayRandomElement(string[] someArray)
-{
-    Random rnd = new Random();
-    int randomIndex = rnd.Next(0, someArray.Length);
-    Console.WriteLine(someArray[randomIndex]);
-}
-
-
 static void ReadFromFile(string filePath)
 {
     if (File.Exists(filePath))
     {
         string[] tempArray = File.ReadAllLines(filePath);
         Console.WriteLine($"There are {tempArray.Length} lines in the file.");
-        Console.WriteLine("Do you want to see all lines or pick a random one? all/random");
-        string userChoice = Console.ReadLine();
+        Console.WriteLine("Do ylu want to sea all lines or pick a random one? all/random");
+        string userChoise = Console.ReadLine();
 
-        if (userChoice == "all")
+        if (userChoise == "all")
         {
-            DisplayDataFromArray(tempArray);
+            DisplayDataFromArry(tempArray);
         }
-        else if (userChoice == "random")
+        else if (userChoise == "random")
         {
             DisplayRandomElement(tempArray);
         }
@@ -45,11 +36,16 @@ static void ReadFromFile(string filePath)
             Console.WriteLine("Try again.");
         }
 
-
     }
     else
     {
         Console.WriteLine($"File was not found.");
     }
+}
 
+static void DisplayRandomElement(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    Console.WriteLine(someArray[randomIndex]);
 }
